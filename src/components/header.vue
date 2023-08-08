3ecdccb8170d643637e01a82e0eabc99dd960b78
= <=>
2023-08-08 17:48:49 +0200

    new navbar

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,22 @@
 <template>
     <main>
-        <nav class="navbar ">
-
-        </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Feverest</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" href="#">Free Games</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Fuel Calculator</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
     </main>
 </template>
