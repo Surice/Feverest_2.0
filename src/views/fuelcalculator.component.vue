4afc6d8f1f26ae1d5d95bc6d082686a7822517f4
= <=>
2023-08-31 06:35:51 +0200

    first begining of fuel calc

--- src/views/fuelcalculator.component.vue
+++ src/views/fuelcalculator.component.vue
@@ -1,1 +1,9 @@
-<template></template>
+<template>
+    <main>
+        <div class="row">
+            <div class="col-sm1"></div>
+            <div class="col-sm11"></div>
+        </div>
+        <div class="container p-5"></div>
+    </main>
+</template>
