ddacbecfff033c885906641e6c77bf1a94af7c42
= <=>
2024-09-27 22:28:15 +0300

    add function to fuel calculator

--- src/views/fuelcalculator.component.vue
+++ src/views/fuelcalculator.component.vue
@@ -12,7 +12,7 @@
                         <div class="row">
                             <div class="col">
                                 <div class="input-group mb-3 border rounded shadow">
-                                    <input type="number" class="form-control  input-input text-end border-0">
+                                    <input type="number" id="hrs_dur" v-on:change="calcDuration(); calcFuel()" min="0" class="form-control  input-input text-end border-0">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text input-span border-0">hrs</span>
                                     </div>
@@ -20,7 +20,7 @@
                             </div>
                             <div class="col">
                                 <div class="input-group mb-3 border rounded shadow">
-                                    <input type="number" class="form-control  input-input text-end border-0">
+                                    <input type="number" id="min_dur" v-on:change="calcDuration(); calcFuel()" min="0" step="1" class="form-control  input-input text-end border-0">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text input-span border-0">min</span>
                                     </div>
@@ -32,7 +32,7 @@
                 <div class="my-4">
                     <h5 class="subheadline">Lap Time</h5>
                     <div class="input-group mb-3 border rounded shadow">
-                        <input type="number" class=" input-input form-control text-end border-0" min="0">
+                        <input type="number" id="lap_time" v-on:change="calcLaptime(); calcFuel()" class=" input-input form-control text-end border-0" min="0">
                         <div class="input-group-prepend">
                                 <span class="input-group-text input-span border-0">MM.ss</span>
                         </div>
@@ -41,7 +41,7 @@
                 <div class="my-4">
                     <h5 class="subheadline">Lap Consumption</h5>   
                     <div class="input-group mb-3 border rounded shadow">
-                        <input type="number" class=" input-input form-control text-end border-0" min="0">
+                        <input type="number" id="lap_cons" v-on:change="calcFuel()" class=" input-input form-control text-end border-0" min="0">
                         <div class="input-group-prepend">
                                 <span class="input-group-text input-span border-0">liters</span>
                         </div>
@@ -50,7 +50,7 @@
                 <div class="my-4">
                     <h5 class="subheadline">Fuel Consumption</h5>   
                     <div class="input-group mb-3 border rounded shadow deactivated">
-                        <input type="number" class=" input-input form-control text-end border-0 deactivated" placeholder="12" aria-label="e.g.: 1.09" aria-describedby="basic-addon2" min="0" readonly>
+                        <input type="number" id="fuel_cons" class=" input-input form-control text-end border-0 deactivated" value="0" min="0" readonly>
                         <div class="input-group-prepend">
                                 <span class="input-group-text input-span border-0 deactivated">liters</span>
                         </div>
@@ -60,6 +60,74 @@
         </div>
     </main>
 </template>
+<script>
+import { toValue } from 'vue';
+
+    export default {
+        data: () => {
+            return{
+                race_dur: 0,
+                lap_sec: 0,
+                laps: 0,
+                fuel: 0
+            }
+        },
+        methods: {
+            writeInput(){
+                
+            },
+            calcDuration(){
+                // calculate the race distance in secondes
+                
+                let hrs_dur = document.getElementById("hrs_dur").value;
+                let min_dur = document.getElementById("min_dur").value;
+
+                this.race_dur = hrs_dur * 60 * 60 + min_dur * 60;
+                console.log("race distance: " + this.race_dur);
+            },
+            calcLaptime(){
+                // calculate the input laptime in secondes
+
+                let lap_time = document.getElementById("lap_time").value;
+                const lap_time_arr = lap_time.split(".");
+
+                if (lap_time_arr[1] == null) {
+                    this.lap_sec = Number(lap_time_arr[0])
+                } else {
+                    this.lap_sec = Number(lap_time_arr[0] * 60) + Number(lap_time_arr[1]);
+                }
+
+                console.log("lap time in secondes: " + this.lap_sec);
+            },
+            calcFuel(){
+                // calculate the raced laps based on duration and laptime
+
+                let lap_cons = document.getElementById("lap_cons").value;
+                const laps_floor = Math.floor(this.race_dur / this.lap_sec);
+                const diff = (this.race_dur / this.lap_sec) - laps_floor;
+
+                if (this.race_dur != 0 && this.lap_sec != 0) {
+                    
+                    console.log("laps: " + this.race_dur / this.lap_sec);
+                    
+                    if (diff >= .7) {
+                        this.laps = Math.ceil(this.race_dur / this.lap_sec) + 2;
+                    } else {
+                        this.laps = Math.ceil(this.race_dur / this.lap_sec) + 1; 
+                    }
+                
+                    this.fuel = lap_cons * this.laps;
+
+                    document.getElementById("fuel_cons").value = Math.ceil(this.fuel);
+
+                    console.log("raced laps: " + this.laps)
+                    console.log("fuel per race: " + Math.ceil(this.fuel))
+                }
+            }
+        }
+    }
+
+</script>
 <style>
     .div-width{
         width: 28vw;
@@ -101,5 +169,6 @@
     }
     .deactivated{
         background-color: lightgray !important;
+        color: #212529 !important;
     }
 </style>
